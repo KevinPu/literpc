122e8d3e6b7b5049875304ff530f2b1ca6a6bcc0
Kevin Pu <pukaib@foxmail.com>
2018-02-23 00:05:05 +0800

    m

---
+++ literpc-demo/literpc-demo-api/src/main/java/io/literpc/demo/api/LiterpcDemoApiApplication.java
@@ -0,0 +1,12 @@
+package io.literpc.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterpcDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterpcDemoApiApplication.class, args);
+	}
+}
---
+++ literpc-demo/literpc-demo-provider/src/main/java/io/literpc/demo/provider/LiterpcDemoProviderApplication.java
@@ -0,0 +1,12 @@
+package io.literpc.demo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterpcDemoProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterpcDemoProviderApplication.class, args);
+	}
+}
